40897d3a74a48f6038409f99906beb9a0ff4d119
Gr-an-t <162178598+Gr-an-t@users.noreply.github.com>
2024-07-03 17:01:19 -0500

    Created test button and set up zip logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.IO.Compression;
 
 namespace Bionic_Converter
 {
@@ -19,6 +21,44 @@ namespace Bionic_Converter
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void documentSelectButton_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+			dlg.DefaultExt = ".epub";
+			dlg.Filter = "EPUB Files (*.epub)|*.epub";
+
+			Nullable<bool> result = dlg.ShowDialog();
+			if (result.HasValue && result.Value)
+			{
+				string fileName = System.IO.Path.GetFileName(dlg.FileName);
+				string extractedPath = System.IO.Path.GetFullPath(dlg.FileName);
+				File.Move(extractedPath, System.IO.Path.ChangeExtension(extractedPath, ".zip"));
+				extractedPath = System.IO.Path.ChangeExtension(extractedPath, ".zip");
+				string outputPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(extractedPath) ?? "C:\\", fileName);
+				string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), Guid.NewGuid().ToString());
+				ZipFile.ExtractToDirectory(extractedPath, tempPath);
+
+				//TODO Modify zip files
+				//if (!ConvertEpubFiles(tempPath)) return;
+
+				ZipFile.CreateFromDirectory(tempPath, outputPath);
+
+
+				//TODO convert zip to epub
+
+				if (Directory.Exists(tempPath))
+				{
+					Directory.Delete(tempPath, true);
+				}
+			}
+		}
+
+		static private bool ConvertEpubFiles(string fileLocation)
+		{
+			return true;
 		}
 	}
 }
